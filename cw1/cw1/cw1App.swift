1af95cc48e148936103455af7c486a96e6930971
Ahmad Alhashemi <ahmad@ahmadh.com>
2022-09-05 19:32:28 +0300

    empty project

---
+++ cw1/cw1/cw1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw1App.swift
+//  cw1
+//
+//  Created by Ahmad Alhashemi on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
